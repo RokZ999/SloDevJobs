e635dd1ad2a75097c40f9c29deab58531d6e4cc2
Rok Zabret <71169333+RokZ999@users.noreply.github.com>
2024-11-24 17:17:36 +0100

    Update QuickSummary.svelte

--- src/lib/components/QuickSummary.svelte
+++ src/lib/components/QuickSummary.svelte
@@ -11,12 +11,13 @@
 	const jobsCount: number = data.length;
 
 	const salaries: number[] = data
-		.map((job) => job.normalized_salary)
-		.filter((salary) => salary !== null && salary !== undefined);
+		.map((job) => Number(job.normalized_salary)) 
+		.filter((salary) => !isNaN(salary) && salary !== null && salary !== undefined);
 
 	const salaries_yearly: number[] = data
-		.map((job) => job.normalized_salary_yearly)
-		.filter((salary) => salary !== null && salary !== undefined);
+		.map((job) => Number(job.normalized_salary_yearly)) 
+		.filter((salary) => !isNaN(salary) && salary !== null && salary !== undefined);
+
 
 	const mediana_monthly = median(salaries);
 	const mediana_yearly = median(salaries_yearly);
@@ -31,7 +32,6 @@
 
 		return sorted[middle];
 	}
-	debugger;
 </script>
 
 <div class="flex justify-center space-x-1">
